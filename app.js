228223fcdd13367f6bd34bf03a441a9510f62b88
Fernando Tavera <fer8707@gmail.com>
2021-05-14 12:05:06 -0500

    update

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+//const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 app.use(function(req, res, next){
   res.locals.currentUser = req.session.currentUser;
